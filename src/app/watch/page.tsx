c73935c66305a01025a97521afbbd7f1f4426b53
MeSkipperr <ikadekyolaandika02@gmail.com>
2025-03-31 17:40:21 +0800

    v-1.2.1 [selected video] - page

--- src/app/watch/page.tsx
+++ src/app/watch/page.tsx
@@ -1,6 +1,18 @@
-const SelectedVideo = () => {
+const SelectedVideo = async () => {
     return ( 
-        <div className=""></div>
+        <div className="w-full flex  h-dvh bg-slate-300 gap-4">
+            <div className="w-2/3 h-full bg-amber-400 p-4">
+                <div className="w-full aspect-video bg-teal-400 rounded-lg relative overflow-hidden">
+                    <video 
+                        autoPlay
+                        controls
+                        className="w-full h-full object-cover absolute inset-0"
+                        src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+                    />
+                </div>
+            </div>
+            <div className="w-1/3 h-full bg-green-300"></div>
+        </div>
     );
 }
 
